8e61b8918275d95dbbd1a205202c4e5f31018d23
atif-bakhsh <atif.bakhsh@gmail.com>
2023-05-17 16:32:31 +0500

    Added private and encapsulations.

--- src/Campus.java
+++ src/Campus.java
@@ -2,9 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Campus {
-    String name;
-    String address;
-    Directory directory;
+    private String name;
+    private String address;
+    private Directory directory;
+
+    public Campus(String name, String address, Directory directory, ArrayList<Department> departments) {
+        this.name = name;
+        this.address = address;
+        this.directory = directory;
+        this.departments = departments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Directory getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(Directory directory) {
+        this.directory = directory;
+    }
+
+    public ArrayList<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(ArrayList<Department> departments) {
+        this.departments = departments;
+    }
+
     ArrayList<Department> departments;
     public void addDepartment(Department department) {
 
