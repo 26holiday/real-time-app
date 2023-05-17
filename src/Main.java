3e89df6b74e9d70422e2903b4ddd43c0b984f3b9
atif-bakhsh <atif.bakhsh@gmail.com>
2023-05-17 17:04:48 +0500

    Menu.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,97 @@
+import java.util.Scanner;
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        menu();
+    }
+    public static void menu() {
 
+        System.out.println("-----------------------------------------------");
+        System.out.println("------Welcome to University Maagment System----");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Enter a command: \n");
+        System.out.println("1 - Employee             2 - Lab");
+        System.out.println("3 - Computer             4 - Department");
+        System.out.println("              0 - Exit");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println(" 1 - New Employee.     2 - Search Employee");
+                System.out.println("           3 - Remove Employee");
+                choice = scanner.nextInt();
+                if(choice == 1){
+
+                } else if (choice == 2) {
+
+                }
+                else if (choice == 3) {
+
+                }
+                menu();
+                break;
+            case 2:
+                System.out.println(" 1 - New Lab.     2 - Search Lab");
+                System.out.println("        3 - Remove Lab");
+                choice = scanner.nextInt();
+                if(choice == 1){
+
+                } else if (choice == 2) {
+
+                }
+                else if (choice == 3) {
+
+                }
+                choice = scanner.nextInt();
+                if(choice == 1){
+
+                } else if (choice == 2) {
+
+                }
+                else if (choice == 3) {
+
+                }
+                menu();
+                break;
+            case 3:
+                System.out.println(" 1 - New PC.     2 - Search PC");
+                System.out.println("           3 - Remove PC");
+                choice = scanner.nextInt();
+                if(choice == 1){
+
+                } else if (choice == 2) {
+
+                }
+                else if (choice == 3) {
+
+                }
+                menu();
+                break;
+            case 4:
+                System.out.println(" 1 - New Department.     2 - Search Department");
+                System.out.println("        3 - Remove Department");
+                choice = scanner.nextInt();
+                if(choice == 1){
+
+                } else if (choice == 2) {
+
+                }
+                choice = scanner.nextInt();
+                if(choice == 1){
+
+                } else if (choice == 2) {
+
+                }
+                else if (choice == 3) {
+
+                }
+                menu();
+                break;
+            case 0:
+                System.out.println("Exiting University Management System. Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+                break;
+        }
     }
 }
