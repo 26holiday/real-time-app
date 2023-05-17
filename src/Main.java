226ef37b21c66182f85f005bb3cea9126bbc58e2
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-05-17 16:59:47 +0500

    added changes to the app

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        University university = new University(null, null);
+        Campus campus = new Campus(null, null, null, null);
+        Department department = new Department(null, null);
+        HOD hod = new HOD(null, null);
+        Director director = new Director(null, null);
+        Employee employee = new Employee(null, null);
+        Lab lab = new Lab(null, false, null);
     }
 }
