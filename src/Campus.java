7190e342164496374a033bf5dc5ffbe82ab54622
atif-bakhsh <atif.bakhsh@gmail.com>
2023-05-17 16:15:37 +0500

    I have added the Class Campus.jav

---
+++ src/Campus.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campus {
+    String name;
+    String address;
+    Directory directory;
+    ArrayList<Department> departments;
+    public void addDepartment(Department department) {
+
+    }
+    public void removeDepartment(Department department){
+
+    }
+    public Department getDepartment() {
+
+    }
+}
