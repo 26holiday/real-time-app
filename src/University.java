c3d664111079b723497de167d5d4c1ccdef9180d
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-05-17 17:01:35 +0500

    added changes

--- src/University.java
+++ src/University.java
@@ -3,6 +3,43 @@ import java.util.ArrayList;
 public class University {
     String name;
     ArrayList<Campus> campuses;
-    
+
+    University(String name, ArrayList<Campus> campuses) {
+        this.name = name;
+        this.campuses = campuses;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Campus> getCampuses() {
+        return campuses;
+    }
+
+    public void setCampuses(ArrayList<Campus> campuses) {
+        this.campuses = campuses;
+    }
+
+
+    public void addCampus(Campus campus) {
+        campuses.add(campus);
+    }
+
+    public void removeCampus(Campus campus) {
+        campuses.remove(campus);
+    }
+
+    public ArrayList<Campus> getCampus() {
+        return campuses;
+    }
+
+
+
     
 }
