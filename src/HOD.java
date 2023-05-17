3da868ad70369ec1b767831acc7b2f8441de8cf6
26holiday <your-github-email@example.com>
2023-05-17 16:29:08 +0500

    pushing

---
+++ src/HOD.java
@@ -0,0 +1,15 @@
+public class HOD {
+    private Employee hodDetails;
+
+    public HOD(Employee hodDetails) {
+        this.hodDetails = hodDetails;
+    }
+
+    public Employee getHodDetails() {
+        return hodDetails;
+    }
+
+    public void setHodDetails(Employee hodDetails) {
+        this.hodDetails = hodDetails;
+    }
+}
---
+++ src/LabStaff.java
@@ -0,0 +1,15 @@
+public class LabStaff {
+    private Employee staffDetails;
+
+    public LabStaff(Employee staffDetails) {
+        this.staffDetails = staffDetails;
+    }
+
+    public Employee getStaffDetails() {
+        return staffDetails;
+    }
+
+    public void setStaffDetails(Employee staffDetails) {
+        this.staffDetails = staffDetails;
+    }
+}
